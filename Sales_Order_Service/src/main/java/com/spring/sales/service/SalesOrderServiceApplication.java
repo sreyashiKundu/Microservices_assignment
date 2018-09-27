e41fc15a43ce81ab20184e677d2e766d4303a837
509036 <509036@PC303660.cts.com>
2018-09-27 13:51:41 +0530

    push all services to Github

---
+++ Sales_Order_Service/src/main/java/com/spring/sales/service/SalesOrderServiceApplication.java
@@ -0,0 +1,12 @@
+package com.spring.sales.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesOrderServiceApplication.class, args);
+	}
+}
