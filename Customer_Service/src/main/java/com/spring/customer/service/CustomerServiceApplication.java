12b18405aff0986db6211e9114d1d32ea8c8ce26
509036 <509036@PC303660.cts.com>
2018-10-05 09:56:17 +0530

    customer service

--- Customer_Service/src/main/java/com/spring/customer/service/CustomerServiceApplication.java
+++ Customer_Service/src/main/java/com/spring/customer/service/CustomerServiceApplication.java
@@ -2,8 +2,11 @@ package com.spring.customer.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
